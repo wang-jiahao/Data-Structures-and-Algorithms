d7010397ad03dd64f014bf07cecd836e1630649c
Wangjiahao <2328796228@qq.com>
2024-10-22 21:37:42 +0800

    hw4 hw5

---
+++ hw5/topK.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    cin.get();
+    string s;
+    getline(cin, s);
+    istringstream iss(s);
+    int arr[100000];
+    int i = 0;
+    while (iss >> arr[i]) {
+        i++;
+    }
+    sort(arr, arr + i);
+    cout << arr[i - k] << endl;
+
+    return 0;
+}
